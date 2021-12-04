b64300131a1ce07377d2dd4f7e137feb76952da1
spymb <spym1933@foxmail.com>
2021-12-04 20:33:50 +0800

    项目初始化并搭建好HTML

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="topNav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>UI</h1>
+      <h2>一个UI 框架</h2>
+      <p class="actions">
+        <a>GitHub</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
