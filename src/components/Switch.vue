88478aa3683849bcdd26f85b96f47e28aabadfe0
spymb <spym1933@foxmail.com>
2021-12-06 00:02:51 +0800

    设置Switch的HTML和CSS

--- src/components/Switch.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    Switch
-  </div>
-</template>
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
 import {createWebHashHistory, createRouter} from 'vue-router';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
-import Switch from './components/Switch.vue';
-import Button from './components/Button.vue';
-import Dialog from './components/Dialog.vue';
-import Tabs from './components/Tabs.vue';
+import SwitchPage from './components/SwitchPage.vue';
+import ButtonPage from './components/ButtonPage.vue';
+import DialogPage from './components/DialogPage.vue';
+import TabsPage from './components/TabsPage.vue';
 
 const history = createWebHashHistory();
 export const router = createRouter({
@@ -14,10 +14,10 @@ export const router = createRouter({
     {
       path: '/doc', component: Doc,
       children: [
-        {path: 'switch', component: Switch},
-        {path: 'button', component: Button},
-        {path: 'dialog', component: Dialog},
-        {path: 'tabs', component: Tabs},
+        {path: 'switch', component: SwitchPage},
+        {path: 'button', component: ButtonPage},
+        {path: 'dialog', component: DialogPage},
+        {path: 'tabs', component: TabsPage},
       ]
     },
   ]
