e58d23bc626d8fb8b84587dbc5da92a1d90fe11e
spymb <spym1933@foxmail.com>
2021-12-05 14:10:02 +0800

    实现aside的显示隐藏

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,12 +1,26 @@
 <template>
-    <div class="topNav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+  <div class="topNav">
+    <div class="logo" @click="toggleAside">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+  </div>
 </template>
+
+<script lang="ts">
+import {inject, Ref} from 'vue';
+
+export default {
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('asideVisible');
+    const toggleAside = function () {
+      asideVisible.value = !asideVisible.value;
+    };
+    return {toggleAside};
+  }
+};
+</script>
 
 <style lang="scss" scoped>
 .topNav {
@@ -15,14 +29,17 @@
   background: pink;
   display: flex;
   padding: 16px;
+
   > .logo {
     max-width: 6em;
     margin-right: auto;
   }
+
   > .menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
+
     > li {
       margin: 0 1em;
     }
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,7 @@
   <div>
     <TopNav/>
     <div class="content">
-      <aside>
+      <aside v-if="asideVisible">
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -26,8 +26,13 @@
 
 <script lang="ts">
 import TopNav from '../components/TopNav.vue'
+import {inject, Ref} from 'vue';
 export default {
-  components: {TopNav}
+  components: {TopNav},
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('asideVisible');
+    return {asideVisible}
+  }
 }
 </script>
 
