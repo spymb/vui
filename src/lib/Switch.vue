0dc8a7e692cfdd0d945736540d8a782d6c045131
spymb <spym1933@foxmail.com>
2021-12-06 00:17:49 +0800

    将Switch设计成点击按钮

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,30 +1,50 @@
 <template>
-  <button>
+  <button @click="toggle" :class="{checked}">
     <span></span>
   </button>
 </template>
 
+<script lang="ts">
+import {ref} from 'vue';
+
+export default {
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value;
+    };
+    return {checked, toggle};
+  }
+};
+</script>
+
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
-button{
+button {
   height: $h;
   width: $h*2;
   border: none;
-  background: blue;
+  background: grey;
   border-radius: $h/2;
   position: relative;
 }
-span{
+
+span {
   position: absolute;
   top: 2px;
   left: 2px;
   height: $h2;
   width: $h2;
-  background:white;
+  background: white;
   border-radius: $h2 / 2;
 }
-button:hover > span {
+
+button.checked {
+  background: blue;
+}
+
+button.checked > span {
   left: calc(100% - #{$h2} - 2px);
 }
 </style>
