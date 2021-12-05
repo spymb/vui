0133a37a1cf78705bc30b3cf9e14314f499d2010
spymb <spym1933@foxmail.com>
2021-12-06 00:21:07 +0800

    给Switch按钮添加动画

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -38,6 +38,7 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
 
 button.checked {
@@ -46,5 +47,9 @@ button.checked {
 
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+  outline: none;
 }
 </style>
