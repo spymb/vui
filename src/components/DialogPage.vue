22a357d0eaf6a69d1d11b778c2d86f4095ed2a6a
spymb <spym1933@foxmail.com>
2021-12-07 20:04:51 +0800

    创建并初始化Dialog组件

--- src/components/DialogPage.vue
+++ src/components/DialogPage.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    dialog
+    <Dialog/>
   </div>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue';
+export default {
+  components: {Dialog}
+};
+</script>
