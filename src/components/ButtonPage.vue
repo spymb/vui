a9d73aee6e89d3209ebfb37c99aa6a125ae44e13
spymb <spym1933@foxmail.com>
2021-12-07 12:36:42 +0800

    Button支持size属性

--- src/components/ButtonPage.vue
+++ src/components/ButtonPage.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    button
-    <span>
+    <h1>theme demo</h1>
+    <div>
       <Button>b1</Button>
-      <Button theme="button">b2</Button>
-      <Button theme="link">b3</Button>
-      <Button theme="text">b4</Button>
-    </span>
+      <Button theme="link">b2</Button>
+      <Button theme="text">b3</Button>
+    </div>
+    <h1>size demo</h1>
+    <div>
+      <Button size="big">s1</Button>
+      <Button>s2</Button>
+      <Button size="small">s3</Button>
+    </div>
   </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,32 @@
 <template>
-  <button class="gulu-button" :class=" { [`gulu-theme-${theme}`]: theme } ">
+  <button class="gulu-button" :class="classes">
     <slot/>
   </button>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue';
+
 export default {
   props: {
     theme: {
       type: String,
       default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'normal'
     }
+  },
+  setup(props) {
+    const {theme, size} = props;
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size,
+      };
+    });
+    return {classes};
   }
 };
 </script>
@@ -35,34 +51,52 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+
   & + & {
     margin-left: 8px;
   }
-  &:hover,
-  &:focus {
+
+  &:hover, &:focus {
     color: $blue;
     border-color: $blue;
-  }
-  &:focus {
     outline: none;
   }
+
   &::-moz-focus-inner {
     border: 0;
   }
-  &.gulu-theme-link{
+
+  &.gulu-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
-    &:hover,&:focus{
+
+    &:hover, &:focus {
       color: lighten($blue, 10%);
     }
   }
-  &.gulu-theme-text{
+
+  &.gulu-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
-    &:hover,&:focus{
+
+    &:hover, &:focus {
       background: darken(white, 5%);;
+    }
+  }
+
+  &.gulu-theme-button {
+    &.gulu-size-big {
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px
+    }
+
+    &.gulu-size-small {
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
     }
   }
 }
