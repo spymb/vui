0dc3a09415a4984681699d0a89c0db1a98d64c3f
spymb <spym1933@foxmail.com>
2021-12-09 20:00:09 +0800

    实现Tab的切换

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,14 +1,25 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index"/>
+ <div class="gulu-tabs">
+  <div class="gulu-tabs-nav">
+    <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index"
+         @click="select(t)" :class="{selected: t=== selected}">{{t}}</div>
   </div>
+  <div class="gulu-tabs-content">
+    <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :key="index" :is="c"
+               :class="{selected: c.props.title === selected }"/>
+  </div>
+</div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
 
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
@@ -19,10 +30,48 @@ export default {
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
     return {
       defaults,
-      titles
+      titles,
+      select,
     };
   }
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    text-align: left;
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
+</style>
