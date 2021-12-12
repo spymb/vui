efa8c4d33b701b76c7eb26cda1428e1130a60249
spymb <spym1933@foxmail.com>
2021-12-12 18:47:27 +0800

    抽离出Demo组件

--- src/components/SwitchPage.vue
+++ src/components/SwitchPage.vue
@@ -1,31 +1,8 @@
 <template>
   <div>
     <h1>Switch 组件示例 </h1>
-    <div class="demo">
-      <h2>常规用法</h2>
-      <div class="demo-component">
-        <component :is="SwitchDemo1" />
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre class="language-html" v-html="Prism.highlight(SwitchDemo1.__sourceCode, Prism.languages.html, 'html')" />
-      </div>
-    </div>
-
-    <div class="demo">
-      <h2>支持 disabled </h2>
-      <div class="demo-component">
-        <component :is="SwitchDemo2" />
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre class="language-html" v-html="Prism.highlight(SwitchDemo2.__sourceCode, Prism.languages.html, 'html')" />
-      </div>
-    </div>
+    <Demo :component="SwitchDemo1"/>
+    <Demo :component="SwitchDemo2"/>
   </div>
 </template>
 
@@ -36,11 +13,12 @@ import SwitchDemo1 from './SwitchDemo1.vue';
 import SwitchDemo2 from './SwitchDemo2.vue';
 import 'prismjs';
 import 'prismjs/themes/prism.css'
+import Demo from './Demo.vue';
 
 const Prism = (window as any).Prism
 
 export default {
-  components: {Button},
+  components: {Demo, Button},
   setup() {
     const bool = ref(false);
     return {
@@ -52,38 +30,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss" scoped>
-$border-color: #d9d9d9;
-
-.demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-
-  > h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-
-  &-component {
-    padding: 16px;
-  }
-
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-
-    > pre {
-      line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
-      margin: 0;
-    }
-  }
-}
-</style>
