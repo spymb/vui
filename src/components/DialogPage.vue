9d7b5729ad948adf4b86624a54d52967b6a2d821
spymb <spym1933@foxmail.com>
2021-12-12 19:47:29 +0800

    重构DialogPage

--- src/components/DialogPage.vue
+++ src/components/DialogPage.vue
@@ -1,53 +1,20 @@
 <template>
-  <div >
-    <Button @click="toggle">显示对话框</Button>
-    <Dialog v-model:visible="x" :closeOnClickOverlay="false"
-            :ok="f1" :cancel="f2">
-      <template v-slot:title>
-        <strong>加粗的标题</strong>
-      </template>
-      <template v-slot:content>
-        <strong>加粗的第一行</strong>
-        <div>未加粗的第二行</div>
-      </template>
-    </Dialog>
-    <Button @click="showDialog">showDialog</Button>
-  </div>
+  <div>Dialog 示例</div>
+  <Demo :component="DialogDemo1" />
+  <Demo :component="DialogDemo2" />
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue';
-import Button from '../lib/Button.vue';
-import {h, ref} from 'vue';
-import {openDialog} from '../lib/openDialog';
+import Demo from './Demo.vue';
+import DialogDemo1 from './demos/DialogDemo1.vue';
+import DialogDemo2 from './demos/DialogDemo2.vue';
 
 export default {
-  components: {Button, Dialog},
+  components: {Demo},
   setup() {
-    const x = ref(false);
-    const toggle = () => {
-      x.value = !x.value;
-    };
-    const f1 = () => {
-      return false;
-    };
-    const f2 = () => {
-    };
-    const showDialog = () => {
-      openDialog({
-        title: h('strong', {}, '标题'),
-        content: '你好',
-        ok() {
-          console.log('ok');
-          },
-        cancel() {
-          console.log('cancel');
-        },
-      })
-    }
     return {
-      x, toggle, f1, f2, showDialog
-    };
+      DialogDemo1, DialogDemo2
+    }
   }
-};
+}
 </script>
---
+++ src/components/demos/DialogDemo1.vue
@@ -0,0 +1,49 @@
+<demo>
+常规使用
+</demo>
+
+<template>
+  <div>
+    <Button @click="toggle">打开对话框</Button>
+    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+      <template v-slot:title>
+        <strong>加粗的标题</strong>
+      </template>
+      <template v-slot:content>
+        <strong>加粗的第一行</strong>
+        <div>第二行</div>
+      </template>
+    </Dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import Dialog from '../../lib/Dialog.vue'
+import Button from '../../lib/Button.vue'
+import {
+  ref
+} from 'vue'
+
+export default {
+  components: {
+    Dialog,
+    Button
+  },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    const f1 = () => {
+      return false
+    }
+    const f2 = () => {}
+    return {
+      x,
+      toggle,
+      f1,
+      f2
+    }
+  }
+}
+</script>
