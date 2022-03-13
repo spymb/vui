529d1f86627c152ed1daf9893e981afef52c8cb4
spymb <spym1933@foxmail.com>
2022-03-13 12:45:14 +0800

    添加ToastMaker

---
+++ src/lib/toast/toast.d.ts
@@ -0,0 +1,10 @@
+export type ToastType = 'normal' | 'secondary' | 'success' | 'warning' | 'error';
+export interface ToastProps  {
+  text?: string;
+  type?: ToastType;
+  delay?: number;
+  center?: boolean;
+  showClose?: boolean;
+  top?: number;
+  id?: string;
+}
