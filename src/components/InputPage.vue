140a02269c4f24c7f40e50171b45670acaced058
spymb <spym1933@foxmail.com>
2022-03-16 00:49:10 +0800

    成功展示size，maxInputLength,label

--- src/components/InputPage.vue
+++ src/components/InputPage.vue
@@ -3,6 +3,9 @@
   <Demo :component="InputDemoDefault" />
   <Demo :component="InputDemoPassword" />
   <Demo :component="InputDemoClear" />
+  <Demo :component="InputDemoLength" />
+  <Demo :component="InputDemoSize" />
+  <Demo :component="InputDemoLabel" />
 </template>
 
 <script lang="ts">
@@ -10,14 +13,22 @@ import Demo from './Demo.vue';
 import InputDemoDefault from './demos/InputDemoDefault.vue';
 import InputDemoPassword from './demos/InputDemoPassword.vue';
 import InputDemoClear from './demos/InputDemoClear.vue';
+import InputDemoLength from './demos/InputDemoLength.vue';
+import InputDemoSize from './demos/InputDemoSize.vue';
+import InputDemoLabel from './demos/InputDemoLabel.vue';
 
 export default {
-  components: {InputDemoClear, InputDemoDefault, Demo, InputDemoPassword},
+  components: {
+    InputDemoLabel, InputDemoSize, InputDemoLength,
+    InputDemoClear, InputDemoDefault, Demo, InputDemoPassword},
   setup() {
     return {
       Demo,InputDemoDefault,
       InputDemoPassword,
-      InputDemoClear
+      InputDemoClear,
+      InputDemoLength,
+      InputDemoSize,
+      InputDemoLabel
     }
   }
 }
---
+++ src/components/demos/InputDemoLength.vue
@@ -0,0 +1,20 @@
+<demo>
+支持 maxLength
+</demo>
+
+<template>
+  <Input v-model:value="value" :maxlength="5"/>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+import Input from '../../lib/Input.vue';
+
+export default {
+  components: {Input},
+  setup() {
+    const value = ref('');
+    return { value };
+  },
+};
+</script>
