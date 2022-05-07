7bad61514d87067dc4adb3126bb8b13023b60fab
spymb <spym1933@foxmail.com>
2022-05-07 19:48:48 +0800

    更新topnav的icon

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="topNav">
-    <router-link to="/" class="logo">
+    <span class="toggler">
+      <svg v-if="togglerVisible" @click="toggleAside">
+        <use xlink:href="#icon-menu"></use>
+      </svg>
+    </span>
+
+    <router-link to="/" class="lng">
       <svg class="icon">
         <use xlink:href="#icon-lunzi"></use>
       </svg>
     </router-link>
-    <svg v-if="togglerVisible" class="toggler" @click="toggleAside">
-      <use xlink:href="#icon-menu"></use>
-    </svg>
-    <ul class="menu">
-      <li>
-        <router-link to="/doc">文档</router-link>
-      </li>
-    </ul>
+
+    <a href="https://github.com/spymb/vui" class="lng">
+      <svg class="icon">
+        <use xlink:href="#icon-github"></use>
+      </svg>
+    </a>
   </div>
 </template>
 
@@ -38,21 +42,17 @@ export default {
 
 <style lang="scss" scoped>
 .topNav {
-  color: #307a04;
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   z-index: 20;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  padding: 8px 16px;
+  padding: 8px 20px;
 
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-
+  .lng{
     > svg {
       width: 32px;
       height: 32px;
@@ -61,36 +61,16 @@ export default {
 
   > .toggler {
     display: none;
-    width: 32px;
-    height: 32px;
-    position: absolute;
-    left: 16px;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-
-    > li {
-      margin: 0 1em;
-    }
   }
 
   @media (max-width: 500px) {
-    > .menu {
-      display: none;
-    }
-    > .logo {
-      margin: 0 auto;
-    }
     > .toggler {
       display: inline-block;
+      > svg {
+        width: 32px;
+        height: 32px;
+      }
     }
   }
-
-
 }
 </style>
